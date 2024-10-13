45253ca21b4542dc6f1f3f864115547c2b072012
Lokeshranjan <lokeshranjan.86@gmail.com>
2024-10-13 19:55:00 +0530

    added hacker rain theme front page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,66 @@
-const Home: React.FC = () => {
-  return <></>;
-};
+import React, { useEffect, useRef } from "react";
+import "../index.css"
 
+const Home: React.FC = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+    const setCanvasSize = () => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    };
+
+    setCanvasSize();
+    window.addEventListener('resize', setCanvasSize);
+
+    const characters = "01";
+    const fontSize = 16;
+    const columns = canvas.width / fontSize;
+    const drops = Array(Math.floor(columns)).fill(1);
+    
+
+    const draw = () => {
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      ctx.fillStyle = '#00ff00';
+      ctx.font = fontSize + 'px monospace';
+
+      drops.forEach((drop, i) => {
+        const text = characters[Math.floor(Math.random() * characters.length)];
+        ctx.fillText(text, i * fontSize, drop * fontSize);
+
+        if (drop * fontSize > canvas.height && Math.random() > 0.975) {
+          drops[i] = 0;
+        }
+
+        drops[i]++;
+      });
+    };
+
+    const interval = setInterval(draw, 33);
+
+    return () => {
+      clearInterval(interval);
+      window.removeEventListener('resize', setCanvasSize);
+    };
+  }, []);
+
+  return (
+    <div className="container">
+      <canvas ref={canvasRef} id="matrix"></canvas>
+      <div className="content">
+        <h1>IIT BHU  Cybersecurity Club</h1>
+        <p>hey developers</p>
+        <button>Join Us</button>
+      </div>
+    </div>
+  );
+};
 export default Home;
